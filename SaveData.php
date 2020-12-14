fc709a79a18e5adaa39363d27464787d45845d25
a.galeazzi <a.galeazzi@herzogdemeuron.com>
2020-12-14 08:59:42 +0100

    Update SaveData.php

--- SaveData.php
+++ SaveData.php
@@ -1,11 +1,11 @@
 <?php
-    if(!isset($_POST["action"])) 
+    if(!isset($_POST["ip"])) 
     {
         echo ("NO DATA RECIEVED");
         return;
     }
 
-    $action = $_POST["action"];
+    $action = $_POST["ip"];
     $date = date('Y/m/d H:i:s');
     $data = "{$action},{$date}\n";
     $myFile = "TestingData.txt";
